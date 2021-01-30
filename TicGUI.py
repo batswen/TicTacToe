9ca95a3a8a266ade7a15aa2dbfada38613e37726
batswen <batswen@gmail.com>
2021-01-30 16:26:33 +0100

    Add spaces for readability

--- TicGUI.py
+++ TicGUI.py
@@ -4,8 +4,8 @@ import Mini
 class Application(Frame):
     def __init__(self,master = None):
         super().__init__(master)
-        self.buttons = [0,0,0, 0,0,0, 0,0,0]
-        self.board = [0,0,0, 0,0,0, 0,0,0]
+        self.buttons = [0, 0, 0,  0, 0, 0,  0, 0, 0]
+        self.board = [0, 0, 0,  0, 0, 0,  0, 0, 0]
         self.master = master
         self.symbol = 'X'
         self.moves = 0
@@ -15,9 +15,9 @@ class Application(Frame):
 
     def create_widgets(self):
         for i in range(9):
-            self.buttons[i]=Button(self.master, height = 3, width = 3, text = i)
+            self.buttons[i] = Button(self.master, height = 3, width = 3, text = i)
             self.buttons[i]['command'] = lambda il = i: self.knopf(il)
-            self.buttons[i].grid(row = i // 3,column = i % 3)
+            self.buttons[i].grid(row = i // 3, column = i % 3)
 
     def knopf(self,n):
         if self.board[n] == 0:
@@ -42,18 +42,18 @@ class Application(Frame):
 
     def test_winner(self):
         "See who's le winner"
-        win_pos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,5,7],[2,5,8],[0,4,8],[2,4,6]]
+        win_pos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 5, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
         for pos in win_pos:
-            if self.board[pos[0]]==self.board[pos[1]]==self.board[pos[2]]==1:
+            if self.board[pos[0]] == self.board[pos[1]] == self.board[pos[2]] == 1:
                 return 'X'
-            if self.board[pos[0]]==self.board[pos[1]]==self.board[pos[2]]==2:
+            if self.board[pos[0]] == self.board[pos[1]] == self.board[pos[2]] == 2:
                 return 'O'
         return ''
 
     def ai(self):
-        move = self.mini.minimax(self.board,2,2)
+        move = self.mini.minimax(self.board, 2, 2)
         return move[1]
-        
+
     def dis(self,dis):
         for i in range(9):
             if dis == True:
